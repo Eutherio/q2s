14885d9eea337f18a671a0e8cb0dd6d5429b7a2b
Lluís Pons <luisponshernandez@gmail.com>
2017-09-01 21:56:59 +0200

    Completes the code to run the first test

--- index.js
+++ index.js
@@ -1,43 +1,112 @@
-const _parseFilter = Symbol('parseFilter');
-const _onError = Symbol('onError');
+const queryString = require('query-string');
 
-module.exports = function(query) {
+module.exports = function(query, table) {
 
-  this.filters = [];
-  this.selection = 'all';
-  this.order = undefined;
+  this.query = query;
+  this.table = table;
   
   let self = this;
 
+  this.translate = function() {
 
-  // Populates the lists of filters,
-  // sets the columns to be selected and in which order
-  function setup () {
+    if ( typeof self.query === 'string' ) {
+      self.query = queryString.parse(self.query);
+    }
 
-    let key;
+    const querySetup = setup(self.query);
 
-    for (key in query) {
+    let whereStmnt = '',
+        orderByStmnt = '',
+        selectStmnt = '';
 
-      if ( key === 'selection') {
-        self.selection = parseSelection(query[key]);
-        continue;
+    if ( querySetup.filters.length > 0 ) {
+      whereStmnt = ' WHERE ';            
+      let i = 0,
+          action;
+      for (let filter of querySetup.filters) {
+  
+        if ( filter.type === 'comparison') {
+          action = '`' + filter.name + '`' + filter.operator + '\'' + filter.value + '\'';
+        }
+        else if ( filter.type === 'pattern' ) {
+          action = '`' + filter.name + '` LIKE \'' + filter.value + '\'';
+        }
+        else if ( filter.type === 'range' ) {
+          action = '`' + filter.name + '` BETWEEN ' + filter.value[0] + ' AND ' + filter.value[1];
+        }
+  
+        whereStmnt += (i === 0 ? '' : filter.junction) + filter.opening + action + filter.closing;
+  
+        if ( querySetup.selection === 'filters') {
+          selectStmnt += (i === 0 ? '' : ', ') + '`' + filter.name + '`';
+        }
+  
+        i++;
       }
-
-      if ( key === 'orderby' ) {
-        self.order = query[key];
-        continue;
-      }
-
-      self.filters.push(parseField(key, query[key]));
 
     }
+
+    // Set SELECT statement in case a list of columns has been specified
+    if ( Array.isArray(querySetup.selection) ) {
+      let j = 0;
+      for (let column of querySetup.selection) {
+        selectStmnt += (j === 0 ? '' : ', ') + '`' + column + '`';
+        j++;
+      }
+    }
+    
+    // Set ORDER BY statement in case a list of columns has been specified
+    if ( querySetup.order ) {
+      orderByStmnt = ' ORDER BY';      
+      let k = 0;
+      for (let item of querySetup.order) {
+        orderByStmnt += (k === 0 ? '' : ', ') + '`' + item.column + '` ' + item.direction;
+        k++;
+      }
+      
+    }
+
+    return 'SELECT ' + selectStmnt + ' FROM ' + self.table + whereStmnt + orderByStmnt;
+
+  };
+
+  // Creates a setup JS object with:
+  // - Lists of filters,
+  // - Columns to select
+  // - Order of the selection
+  function setup (query) {
+
+    let setup = {
+      filters: [],
+      selection: 'all',
+      order: undefined
+    };
+    
+    if ( Object.keys(query).length > 0 ) {
+      for (let key in query) {
+  
+        if ( key === 'selection') {
+          setup.selection = parseSelection(query[key]);
+          continue;
+        }
+  
+        if ( key === 'orderby' ) {
+          setup.order = parseOrder(query[key]);
+          continue;
+        }
+  
+        setup.filters.push(parseFilter(key, query[key]));
+  
+      }
+    }
+
+    return setup;
 
   }
 
   // Transforms the selection parameter value into a
   // list of fields to be selected
   function parseSelection (selection) {
-    let selectionList;
 
     if ( selection !== 'all' && selection !== 'filters') {
       selection = selection.split(',').map((val) => {
@@ -49,9 +118,39 @@ module.exports = function(query) {
 
   }
 
+  // Transforms the orderby parameter value into a
+  // list of fields from which order will be stablished
+  function parseOrder (order) {
+
+    order = order.split(',').map((val) => {
+      val = val.trim();
+      if ( (/\sasc$/i).test(val) ) {
+        return {
+          column: val.slice(0, val.length - 4),
+          direction: 'ASC'
+        }
+      }
+      else if ( (/\sdesc$/i).test(val) ) {
+        return {
+          column: val.slice(0, val.length - 5),
+          direction: 'DESC'
+        }
+      }
+
+      return {
+        column: val,
+        direction: 'ASC'
+      }
+
+    });
+
+    return order;
+
+  }
+
   // Tranform a single field key - value pair into an object
   // to be read when translating to SQL query
-  function parseField (fieldname, value) {
+  function parseFilter (fieldname, value) {
 
     // Default filter object
     let filter = {
@@ -110,6 +209,6 @@ module.exports = function(query) {
 
     return filter;
 
-  };
+  }
 
-}
+};
